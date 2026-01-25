357b8929121411742d54789aefbd8c4ac6a06280
kumarprashant513 <147999914+kumarprashant513@users.noreply.github.com>
2026-01-25 17:10:43 +0530

    Add files via upload

---
+++ backend/constant.tsx
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    "id": "c1d9a8b9-e18d-45d1-b0c8-10c5986196cd",
+    "title": "odoo dashboard",
+    "description": "sjkf",
+    "features": [
+      "chat"
+    ],
+    "images": [
+      "https://live.staticflickr.com/65535/55053976399_66d364caac_k.jpg"
+    ],
+    "price": 38000,
+    "category": "business",
+    "type": "dashboard",
+    "downloadableFileName": "lumina-ebooks.zip"
+  }
+];
